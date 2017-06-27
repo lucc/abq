e8eb04f5d9481991b4ac45731d94552903ddc3aa
Lucas Hoffmann <l-m-h@web.de>
2017-06-27 22:43:52 +0200

    Add option to clear cache

--- lbdb.sh
+++ lbdb.sh
@@ -5,16 +5,19 @@
 prog=${0##*/}
 makefile=${0%.sh}.mk
 version=0.2
-grep_options=( -i -a )
+grep_options=( --ignore-case --text )
+debug=false
 
 usage () {
   echo "$prog [-x] search terms"
+  echo "$prog -c"
   echo "$prog -h"
   echo "$prog -v"
 }
 help () {
   echo "Search the little brothers data base for a matching email address."
   echo "Options: -x for debugging, -h for help, -v for version."
+  echo "With -c the cache is cleared."
   echo "Search terms are used by grep(1) in case insensitive mode."
 }
 grep_chain () {
@@ -27,8 +30,9 @@ grep_chain () {
   fi
 }
 
-while getopts hvx FLAG; do
+while getopts chvx FLAG; do
   case $FLAG in
+    c) echo Clearing cache. >&2; make --file "$makefile" clear-cache --quiet; exit;;
     h) usage; echo; help; exit;;
     v) echo "$prog $version"; echo "Using $(grep --version|head -n 1)"; exit;;
     x) set -x;;
